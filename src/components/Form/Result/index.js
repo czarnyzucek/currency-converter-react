bbfdcbcb4bd3bb81e881b422d4547c97798acff1
Olga Kupryjaniuk <olga.kupryjaniuk@gmail.com>
2021-10-05 17:59:13 +0200

    improved responsiveness

--- src/components/Form/Result/index.js
+++ src/components/Form/Result/index.js
@@ -4,7 +4,8 @@ export const Result = ({ result }) => (
     <p className="form__result">
         {result !== undefined && (
             <>
-                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
+                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
+                {" "}
                 <strong>
                     {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
                 </strong>
