962ed545cf789ed4a58fb69bc06fc55ff2ea5e7c
Olga Kupryjaniuk <olga.kupryjaniuk@gmail.com>
2021-10-13 20:04:45 +0200

    styled-components to Header component added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
-import "./style.css"
+import { Title } from "./styled"
 
 const Header = ({ title }) => (
-    <h1 className="header">{title}</h1>
+    <Title>{title}</Title>
 );
 
 export default Header;
