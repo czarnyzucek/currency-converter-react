ebda66361ed09372a6a63cc055595164e5d2f122
Olga Kupryjaniuk <olga.kupryjaniuk@gmail.com>
2021-10-14 18:33:22 +0200

    Styled-components added to the Container component

--- src/components/Container/styled.js
+++ src/components/Container/styled.js
@@ -1,8 +1,10 @@
-.container {
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
     flex-basis: 600px;
     padding: 5px;
     box-shadow: 0px 0px 78px 30px #000000;
     background-color: hsla(0, 0%, 100%, 0.932);
     border-radius: 10px;
     max-width: 95vw;
-}
+`;
